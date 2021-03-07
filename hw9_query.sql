3988e171a69f34482970b86ea7b21428d24e48a2
chrispope12391 <popex107@umn.edu>
2021-03-07 08:50:42 -0600

    Have finished the hw

---
+++ hw9_query.sql
@@ -0,0 +1,125 @@
+drop table if exists departments;
+drop table if exists dept_emp;
+drop table if exists dept_manager;
+drop table if exists employees;
+drop table if exists salaries;
+drop table if exists titles;
+
+-- created a table to hold the departments csv
+create table departments (
+	dept_no VARCHAR(255),
+	dept_name VARCHAR(255)
+);
+
+-- viewed the data
+select * from departments;
+
+-- created a table to hold the dept_emp csv data
+create table dept_emp(
+	emp_no INT,
+	dept_no VARCHAR(255)
+);
+
+-- viewed dept_emp csv data
+select * from dept_emp;
+
+-- created a table to hold the dept_manager csv data
+create table dept_manager(
+	dept_no VARCHAR(255),
+	emp_no INT
+);
+
+-- viewed dept_manage csv data
+select * from dept_manager;
+
+-- create table to store employees data
+create table employees(
+	emp_no INT,
+	emp_title_id VARCHAR(255),
+	birth_date DATE,
+	first_name VARCHAR(255),
+	last_name VARCHAR(255),
+	sex VARCHAR(255),
+	hire_date DATE
+);
+
+-- Shows the format for the date columns
+show datestyle;
+
+-- view employees csv data
+select * from employees;
+
+-- create table to store salaries csv data
+create table salaries(
+	emp_no INT,
+	salary INT
+);
+
+-- view salaries csv data
+select * from salaries;
+
+-- create table to store titles csv data
+create table titles(
+	title_id VARCHAR (255),
+	title VARCHAR(255)
+);
+
+
+-- Data Analysis #1
+SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
+FROM salaries
+inner join employees on
+employees.emp_no=salaries.emp_no;
+
+-- Data Analysis #2
+select first_name, last_name, hire_date from employees
+where hire_date >= '01-01-1986'
+AND hire_date < '01-01-1987';
+
+-- Data Analysis #3
+select departments.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name,employees.first_name
+from departments
+inner join dept_manager on
+departments.dept_no=dept_manager.dept_no
+inner join employees on
+dept_manager.emp_no=employees.emp_no;
+
+-- Data Analysis #4
+select dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name 
+from employees
+inner join dept_emp on
+employees.emp_no=dept_emp.emp_no
+inner join departments on
+dept_emp.dept_no=departments.dept_no;
+
+-- Data Analysis #5
+select first_name, last_name, sex
+from employees
+where first_name = 'Hercules'
+and last_name like 'B%';
+
+-- Data Analysis #6
+select dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name 
+from employees
+inner join dept_emp on
+employees.emp_no=dept_emp.emp_no
+inner join departments on
+dept_emp.dept_no=departments.dept_no
+where dept_name = 'Sales';
+
+-- Data Analysis #7
+select dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name 
+from employees
+inner join dept_emp on
+employees.emp_no=dept_emp.emp_no
+inner join departments on
+dept_emp.dept_no=departments.dept_no
+where dept_name = 'Sales'
+or dept_name = 'Development';
+
+-- Data Analysis #8
+select last_name, count(last_name) As "Last Name Count"
+from employees
+group by last_name
+order by "Last Name Count" desc;
+
--- hw9_schema.sql
+++ hw9_schema.sql
@@ -1,125 +1,67 @@
-drop table if exists departments;
-drop table if exists dept_emp;
-drop table if exists dept_manager;
-drop table if exists employees;
-drop table if exists salaries;
-drop table if exists titles;
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/5BH5Ls
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
--- created a table to hold the departments csv
-create table departments (
-	dept_no VARCHAR(255),
-	dept_name VARCHAR(255)
+
+CREATE TABLE "departments" (
+    "dept_no" VARCHAR(255)   NOT NULL,
+    "dept_name" VARCHAR(255)   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
 );
 
--- viewed the data
-select * from departments;
-
--- created a table to hold the dept_emp csv data
-create table dept_emp(
-	emp_no INT,
-	dept_no VARCHAR(255)
+CREATE TABLE "dept_emp" (
+    "emp_no" INT   NOT NULL,
+    "dept_no" VARCHAR(255)   NOT NULL
 );
 
--- viewed dept_emp csv data
-select * from dept_emp;
-
--- created a table to hold the dept_manager csv data
-create table dept_manager(
-	dept_no VARCHAR(255),
-	emp_no INT
+CREATE TABLE "dept_manager" (
+    "dept_no" VARCHAR(255)   NOT NULL,
+    "emp_no" INT   NOT NULL
 );
 
--- viewed dept_manage csv data
-select * from dept_manager;
-
--- create table to store employees data
-create table employees(
-	emp_no INT,
-	emp_title_id VARCHAR(255),
-	birth_date DATE,
-	first_name VARCHAR(255),
-	last_name VARCHAR(255),
-	sex VARCHAR(255),
-	hire_date DATE
+CREATE TABLE "employees" (
+    "emp_no" INT   NOT NULL,
+    "emp_title_id" VARCHAR(255)   NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" VARCHAR(255)   NOT NULL,
+    "last_name" VARCHAR(255)   NOT NULL,
+    "sex" VARCHAR(255)   NOT NULL,
+    "hire_date" DATE   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
 );
 
--- Shows the format for the date columns
-show datestyle;
-
--- view employees csv data
-select * from employees;
-
--- create table to store salaries csv data
-create table salaries(
-	emp_no INT,
-	salary INT
+CREATE TABLE "salaries" (
+    "emp_no" INT   NOT NULL,
+    "salary" INT   NOT NULL
 );
 
--- view salaries csv data
-select * from salaries;
-
--- create table to store titles csv data
-create table titles(
-	title_id VARCHAR (255),
-	title VARCHAR(255)
+CREATE TABLE "titles" (
+    "title_id" VARCHAR(255)   NOT NULL,
+    "title" VARCHAR(255)   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     )
 );
 
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
 
--- Data Analysis #1
-SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
-FROM salaries
-inner join employees on
-employees.emp_no=salaries.emp_no;
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
 
--- Data Analysis #2
-select first_name, last_name, hire_date from employees
-where hire_date >= '01-01-1986'
-AND hire_date < '01-01-1987';
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
 
--- Data Analysis #3
-select departments.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name,employees.first_name
-from departments
-inner join dept_manager on
-departments.dept_no=dept_manager.dept_no
-inner join employees on
-dept_manager.emp_no=employees.emp_no;
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
 
--- Data Analysis #4
-select dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name 
-from employees
-inner join dept_emp on
-employees.emp_no=dept_emp.emp_no
-inner join departments on
-dept_emp.dept_no=departments.dept_no;
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "titles" ("title_id");
 
--- Data Analysis #5
-select first_name, last_name, sex
-from employees
-where first_name = 'Hercules'
-and last_name like 'B%';
-
--- Data Analysis #6
-select dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name 
-from employees
-inner join dept_emp on
-employees.emp_no=dept_emp.emp_no
-inner join departments on
-dept_emp.dept_no=departments.dept_no
-where dept_name = 'Sales';
-
--- Data Analysis #7
-select dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name 
-from employees
-inner join dept_emp on
-employees.emp_no=dept_emp.emp_no
-inner join departments on
-dept_emp.dept_no=departments.dept_no
-where dept_name = 'Sales'
-or dept_name = 'Development';
-
--- Data Analysis #8
-select last_name, count(last_name) As "Last Name Count"
-from employees
-group by last_name
-order by "Last Name Count" desc;
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
 
