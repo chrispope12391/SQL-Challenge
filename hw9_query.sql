1d1630f40a61253dfe30da5dad7e5c7cbbdd5afa
chrispope12391 <popex107@umn.edu>
2021-03-07 08:53:37 -0600

    Made minor changes to query file

--- hw9_query.sql
+++ hw9_query.sql
@@ -1,9 +1,9 @@
-drop table if exists departments;
-drop table if exists dept_emp;
-drop table if exists dept_manager;
-drop table if exists employees;
-drop table if exists salaries;
-drop table if exists titles;
+-- drop table if exists departments;
+-- drop table if exists dept_emp;
+-- drop table if exists dept_manager;
+-- drop table if exists employees;
+-- drop table if exists salaries;
+-- drop table if exists titles;
 
 -- created a table to hold the departments csv
 create table departments (
